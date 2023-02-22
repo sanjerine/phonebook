abec060e0c029c8f1ef0395f5d035446e841a3f8
sanjeevan <65218046+sanjerine@users.noreply.github.com>
2023-02-22 18:06:10 +0800

    add backend file

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+morgan.token('content', function (req, res) { 
+  return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
+
+app.use(express.json())
+
+let persons = 
+[
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/', (request, response) => {
+  const date = new Date()
+  const message = 
+  `<h1>Phonebook has info for ${persons.length} people.</h1>
+  ${date}
+  `
+  response.send(message)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  if (persons.map(p => p.name).includes(body.name)) {
+    return response.status(400).json({ 
+      error: 'already in phonebook' 
+    })    
+  }
+  const person = {
+    id: Math.floor(Math.random() * 9999),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+  
+  response.json(person)
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
